758ba326c5a34b16234d05d75c75e37616fde8ea
nicolasjeong <njeong0720@g.ucla.edu>
2021-02-21 23:42:34 -0800

    Checking Image size

--- test/checkImageSize.js
+++ test/checkImageSize.js
@@ -1,13 +1,55 @@
 const sizeOf = require('image-size');
+const sharp = require('sharp');
+const fs = require('fs');
+// Change the code so that the width and height are equal
+// Check width and height separately >=512   <=1024
+// Then add and commit code
 
-var dimensions = sizeOf('./img/team/njeong.jpg');
-if (dimensions.width != 512 && dimensions.height != 512){
-   console.log("Something is wrong")
-   console.log(dimensions.width + " " + dimensions.height) 
+//Actual size of file  <= 1MB  --use fs
+//Add commit code
+
+//Refactor: if either the width or the height are > 1024 or size of file > 1MB --> Resize to 512 by 512
+//Import sharp
+//Add and commit
+
+//First: can you read all the images in folder and print them out
+//Can you apply the function to each image
+//Add and commit
+
+//Can we run this in parallel -- asynchronous
+//Are the libraries that do this already
+
+//bytesize pieces
+//Try to not copy code --> read through the code
+let dimensions = sizeOf('./img/team/njeong.jpg');
+width = dimensions.width;
+height = dimensions.height;
+if (width != height){
+    console.log("Not a pixel-perfect square aspect ratio")
+}
+else{
+    console.log("Good aspect ratio")
+}
+
+if((width <512 || width > 1024) && (height <512 || height > 1024 )){
+    console.log("Image is too small or too big")
+}
+else{
+    console.log("Image size is good")
+}
+
+let imageStats = fs.statSync('./img/team/njeong.jpg');
+let imageFileSizeMB = (imageStats.size) / (1024*1024);
+
+console.log("File is " + imageFileSizeMB + " megabytes");
+
+/*
+if((width!=height) || ((width <512 || width > 1024) && (height <512 || height > 1024 ))){
+    //sharp('./img/team/njeong.jpg')
+    console.log("We need to fix this image")
 }
 
 else{
-    console.log("Image is fine")
-
+    console.log("Image looks great")
 }
-
+*/
